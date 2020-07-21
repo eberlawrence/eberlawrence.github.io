d0cf1b775901e07317876666647762fd27526f33
Eber Lawrence <eberlawrence@hotmail.com>
2020-07-20 21:02:44 -0300

    starting

---
+++ precache-manifest.ddffe4247713b3bd6990069357c2a16c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f54f3d932af42016f7df11a5964103",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1977fca62b7c005c38a3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9a044066c621109156a2",
+    "url": "/static/js/2.86d6ef18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.86d6ef18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1977fca62b7c005c38a3",
+    "url": "/static/js/main.cd4c6dd2.chunk.js"
+  },
+  {
+    "revision": "dfd182da2461ddda8af9",
+    "url": "/static/js/runtime-main.f258c7a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
