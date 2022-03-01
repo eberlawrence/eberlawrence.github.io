c3a5781575b544c6e12eb39bf9c396a2308e034c
eberlawrence <eberlawrence@ufu.br>
2022-03-01 01:15:32 +0000

    Restarting...

---
+++ precache-manifest.b2218a9d289deeafc565d8d508834f61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9090d4d16a26c7b9168884b9e1a78b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3e3ca8a6a12b1a854bb",
+    "url": "/static/css/main.e1ff31e8.chunk.css"
+  },
+  {
+    "revision": "f8c7be89278539119233",
+    "url": "/static/js/2.a285bec5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a285bec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e3ca8a6a12b1a854bb",
+    "url": "/static/js/main.830ce94e.chunk.js"
+  },
+  {
+    "revision": "dfd182da2461ddda8af9",
+    "url": "/static/js/runtime-main.f258c7a8.js"
+  },
+  {
+    "revision": "e66f6b099ed77fc85bdb69fa8b6488bb",
+    "url": "/static/media/github_logo.e66f6b09.svg"
+  },
+  {
+    "revision": "b15b8f765b07122718aba103269eb941",
+    "url": "/static/media/logo.b15b8f76.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30b2f74177bad758dd2048dc0cebaa5b.js"
+  "/precache-manifest.b2218a9d289deeafc565d8d508834f61.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
